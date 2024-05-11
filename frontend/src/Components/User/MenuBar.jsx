94a91f2939cae77a1a7fc5c64552535335046519
Ayush Agrawal <ayushagrawal1811@gmail.com>
2024-05-11 23:13:58 +0530

    Made Menu Bar

--- frontend/src/Components/User/MenuBar.jsx
+++ frontend/src/Components/User/MenuBar.jsx
@@ -1,10 +1,27 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 function MenuBar() {
+    const loc = useLocation();
     return (
         <>
             <Outlet />
-            <div>MenuBar</div>;
+            <div id="menubar">
+                <Link to={``} className={loc.pathname === "/" ? "active" : ""}>
+                    <i className="fa-solid fa-house"></i>
+                </Link>
+                <Link
+                    to={`postblog`}
+                    className={loc.pathname === "/postblog" ? "active" : ""}
+                >
+                    <i className="fa-solid fa-circle-plus"></i>
+                </Link>
+                <Link
+                    to={`profile`}
+                    className={loc.pathname === "/profile" ? "active" : ""}
+                >
+                    <i className="fa-solid fa-user"></i>
+                </Link>
+            </div>
         </>
     );
 }
