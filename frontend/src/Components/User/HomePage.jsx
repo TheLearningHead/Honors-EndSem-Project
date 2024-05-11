2144217e082082a8929937d831c92911ee8f9ed8
Ayush Agrawal <ayushagrawal1811@gmail.com>
2024-05-11 23:34:09 +0530

    Added home page and add blog page

--- frontend/src/Components/User/HomePage.jsx
+++ frontend/src/Components/User/HomePage.jsx
@@ -1,5 +1,44 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 function HomePage() {
-    return <div>HomePage</div>;
+    const [blogList, setBlogList] = useState([]);
+    useEffect(() => {
+        fetchBlogs();
+    }, []);
+
+    const fetchBlogs = async () => {
+        try {
+            const response = await axios.get("http://localhost:8080/getAllBlogs");
+            setBlogList(response.data);
+        } catch (error) {
+            console.error("Error fetching posts:", error);
+        }
+    };
+    const OnePost = () => {
+        return blogList.map((blog, index) => {
+            return (
+                <Link className="card" key={index}>
+                    <div className="body">
+                        <h1>{blog.title}</h1>
+                        <p className="text">{blog.content}</p>
+                        <span class="username">from: @{blog.uploader.username}</span>
+                    </div>
+                </Link>
+            );
+        });
+    };
+
+    if (!blogList) {
+        return <h1>No blogs have been posted yet</h1>;
+    }
+    return (
+        <>
+            <nav>Bloggify</nav>
+            <div id="HomePage">{OnePost()}</div>;
+        </>
+    );
 }
 
 export default HomePage;
--- frontend/src/Components/User/PostBlog.jsx
+++ frontend/src/Components/User/PostBlog.jsx
@@ -1,5 +1,84 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 function PostBlog() {
-    return <div>PostBlog</div>;
+    const [title, settitle] = useState("");
+    const [content, setcontent] = useState("");
+    const history = useNavigate();
+
+    const postBlog = async (e) => {
+        e.preventDefault();
+        if (title.trim() === "" || content.trim() === "") {
+            alert("Please enter title and content.");
+            return;
+        }
+
+        try {
+            // Get token from localStorage
+            const token = localStorage.getItem("token");
+
+            // Make sure token is present
+            if (!token) {
+                alert("User not authenticated. Please login.");
+                return;
+            }
+
+            // Include token in the headers of the POST request
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            };
+
+            // Make a POST request to add the post
+            const response = await axios.post(
+                "http://localhost:8080/addBlog",
+                {
+                    title,
+                    content,
+                },
+                config
+            );
+
+            console.log(response.data); // Log the response data
+
+            // Clear the form after successful submission
+            settitle("");
+            setcontent("");
+            alert("Post added successfully!");
+            history("/");
+        } catch (error) {
+            console.error("Error adding post:", error);
+            alert("Failed to add post. Please try again later.");
+        }
+    };
+
+    return (
+        <div id="PostBlog">
+            <h1>Add a Blog</h1>
+            <form onSubmit={postBlog}>
+                <input
+                    type="text"
+                    value={title}
+                    placeholder="Add title for your Blog"
+                    onChange={(e) => {
+                        settitle(e.target.value);
+                    }}
+                />
+                <input
+                    id="contentdiv"
+                    type="text"
+                    placeholder="Share your story....."
+                    value={content}
+                    onChange={(e) => {
+                        setcontent(e.target.value);
+                    }}
+                />
+                <button className="postblog">Post Blog</button>
+            </form>
+        </div>
+    );
 }
 
 export default PostBlog;
